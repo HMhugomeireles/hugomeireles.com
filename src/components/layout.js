c947765c4a03acde570ef19328381f045334ae52
Hugo Meireles <meireles.hmm@gmail.com>
2019-10-24 23:23:38 +0100

    create data to theme siteconfig menulinks

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+//import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+  const { site: { siteMetadata : { title }} } = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -24,23 +24,9 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+    <> 
+      <p>{title}</p>
+      <main>{children}</main>
     </>
   )
 }
---
+++ src/components/test.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Test = () => (
+  <header>
+    <h1>ffffffffff</h1>
+  </header>
+)
+
+
+export default Test
