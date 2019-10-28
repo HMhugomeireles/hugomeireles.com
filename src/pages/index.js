a092f0fa6b1a80ea65be1a524e1361c8b6d143be
Hugo Meireles <meireles.hmm@gmail.com>
2019-10-28 00:42:15 +0000

    creatre page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import Layout from './../templates/layouts'
+
+export default (props) => (
+    <Layout {...props}>
+      
+    </Layout>
+  )
