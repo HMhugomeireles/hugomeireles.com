7258d2ed9aa892044248826878560f34a1a32dd4
Hugo Meireles <meireles.hmm@gmail.com>
2019-11-07 00:54:18 +0000

    create project list organism/component

---
+++ src/organisms/ProjectList/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ProjectItem from './../../molecules/ProjectItem/'
+
+import { ProjectListSection } from './Styled'
+
+export default function ProjectList() {
+  
+
+  return (
+    <ProjectListSection>
+      <h3>Projects</h3>
+      <ProjectItem 
+        title={'Gatsby hello-world starter 1'}
+        date={'August 24, 2019'}
+        techs={['Javascript', 'Node', 'React']}
+      />
+      <ProjectItem 
+        title={'Gatsby hello-world starter 2'}
+        date={'August 22, 2019'}
+        techs={['Javascript', 'Node', 'React']}
+      />
+      <ProjectItem 
+        title={'Gatsby hello-world starter 3'}
+        date={'August 20, 2019'}
+        techs={['Javascript', 'Node', 'React']}
+      />
+    </ProjectListSection>
+  )
+}
