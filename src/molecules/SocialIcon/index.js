cdbd369b61720c27c1ecc9de8773f2a7468c37d0
Hugo Meireles <meireles.hmm@gmail.com>
2019-11-07 00:56:18 +0000

    change the theme context to inside the social links component

--- src/molecules/SocialIcon/index.js
+++ src/molecules/SocialIcon/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
 import Icon from '../../atoms/Icon'
-
 import GithubLight from './../../images/Github-lightTheme.svg'
 import GithubDark from './../../images/Github-darkTheme.svg'
 import LinkedIn from './../../images/LinkedIN.svg'
 import Twitter from './../../images/Twitter.svg'
+import ThemeContext from './../../context/ThemeContext'
 
-export default ({url, iconName, styleType, darkMode}) => {
+export default ({url, iconName, styleType}) => {
+  const { theme } = useContext(ThemeContext);
   let icon = ''
-  
+
   switch(iconName) {
     case 'GitHub':
-      darkMode ? icon = GithubDark : icon = GithubLight;
+      theme.darkMode ? icon = GithubDark : icon = GithubLight;
       break;
     case 'LinkedIn':
       icon = LinkedIn;
