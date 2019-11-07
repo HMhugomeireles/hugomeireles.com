35c00c04ac081315e9484cda5c17dfbd59596b5e
Hugo Meireles <meireles.hmm@gmail.com>
2019-11-07 00:57:58 +0000

    fixed the styles and props

--- src/organisms/highlightProject/Styled.js
+++ src/organisms/highlightProject/Styled.js
@@ -1,15 +1,22 @@
 import Styled from 'styled-components'
 
 export const HighLightContainer = Styled.article`
+  
   background: #292A37;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   img {
+    width: 100%;
     max-width: 100%;
     margin-bottom: 1.5rem;
+    transition: ease-in-out 400ms;
   }
 
   header, p, footer {
     padding: 1rem 2rem;
+  }
+
+  @media (min-width: 1000px) {
+    max-width: 640px;
   }
 `
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from '../templates/layouts'
 import PageMap from './../molecules/PageMap/'
 import HighLightProject from './../organisms/highlightProject/'
+import ProjectList from './../organisms/ProjectList/'
 import { WrapperSection } from './Styled'
 
 export default () => (
@@ -19,9 +20,8 @@ export default () => (
           }
         ]}}
       />
-      <HighLightProject 
-        
-      />
+      <HighLightProject />
+      <ProjectList />
     </WrapperSection>
   </Layout>
 )
