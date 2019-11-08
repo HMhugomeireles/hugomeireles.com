60d6fd541f050057b97ccc2d8deddeab390a8a3c
Hugo Meireles <meireles.hmm@gmail.com>
2019-11-08 01:41:49 +0000

    move function of tech to the util

---
+++ src/utils/CommonFunc.js
@@ -0,0 +1,14 @@
+export const TechColor = (tech) => {
+  switch(tech){
+    case 'Javascript':
+      return '#FFE600'
+    case 'Node':
+      return '#00FF19'
+    case 'React':
+      return '#0066FF'
+    case 'Express':
+      return '#FF4D00'
+    default:
+      return ''
+  }
+}
