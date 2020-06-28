67197624aa12416f8cc83d29939b63590b739550
HMhugomeireles <meireles.hmm@gmail.com>
2020-06-28 02:20:35 +0100

    create component

---
+++ src/components/Container.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export function Container({ children }) {
+  return <ContainerWrapper>{children}</ContainerWrapper>
+}
---
+++ src/components/Section.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { SectionElement } from "../elements"
+
+export function Section({ children }) {
+  return <SectionElement>{children}</SectionElement>
+}
