60b56da9a524b7ec83a9ab33133236a58fab3e00
HMhugomeireles <meireles.hmm@gmail.com>
2020-06-29 00:38:23 +0100

    add header to container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { ContainerWrapper } from "../elements"
+import { Header } from "./Header"
 
 export function Container({ children }) {
-  return <ContainerWrapper>{children}</ContainerWrapper>
+  return (
+    <ContainerWrapper>
+      <Header />
+      {children}
+    </ContainerWrapper>
+  )
 }
