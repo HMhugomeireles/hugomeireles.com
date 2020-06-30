f8be2175c1302adad9b1132fab7ba89f5d288bb6
HMhugomeireles <meireles.hmm@gmail.com>
2020-07-01 00:15:33 +0100

    change styles on elements

--- src/elements/HeaderElement.js
+++ src/elements/HeaderElement.js
@@ -1,9 +1,9 @@
 import styled from "styled-components"
 
 export const HeaderWrapper = styled.header`
-  position: fixed;
+  position: absolute;
+  top: 0;
   max-width: 1024px;
   width: 100%;
-  left: 25%;
-  top: 2.5rem;
+  margin: 0 auto;
 `
--- src/elements/MenuElements.js
+++ src/elements/MenuElements.js
@@ -4,4 +4,31 @@ export const Menu = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
+  border-radius: 20px 0 0 20px;
+  display: flex;
+  align-items: center;
+
+  background: ${({ theme }) => theme.colors.grey};
+
+  li {
+    display: flex;
+    align-items: center;
+    height: 58px;
+
+    a {
+      text-decoration: none;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.white};
+      padding: 20px;
+
+      ::before {
+        content: "";
+        width: 50px;
+      }
+
+      :hover {
+        opacity: 0.8;
+      }
+    }
+  }
 `
