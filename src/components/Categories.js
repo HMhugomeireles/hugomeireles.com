a2176826684a28a9aa75a8a3b0dfdeff8fe21552
HMhugomeireles <meireles.hmm@gmail.com>
2020-07-04 17:19:21 +0100

    create component icons and categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Icons } from "./Icons"
 
 export function Categories() {
   return (
     <div>
+      <Icons />
       <ul>
         <li>Javascript</li>
         <li>Javascript</li>
